7936468970aac741797a2d73d489795b7bcfd4cd
rice <tonthatanhminhktpm@gmail.com>
2020-08-14 08:18:18 +0700

    minh  CRUD banner API

---
+++ Mp3WebMusic.BAL/Banner/BannerService.cs
@@ -0,0 +1,42 @@
+﻿using Mp3WebMusic.BAL.INTERFACE.Banner;
+using Mp3WebMusic.DAL.INTERFACE.Banner;
+using Mp3WebMusic.DOMAIN.Request_Reponse.Banner;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mp3WebMusic.BAL.Banner
+{
+    public class BannerService : IBannerService
+    {
+        private IBannerRepository bannerRepository;
+        public BannerService(IBannerRepository bannerRepository)
+        {
+            this.bannerRepository = bannerRepository;
+        }
+        public Banners AddBanner(Banners request)
+        {
+            return bannerRepository.AddBanner(request);
+        }
+
+        public Banners DeleteBanner(int id)
+        {
+            return bannerRepository.DeleteBanner(id);
+        }
+
+        public Banners EditBanner(Banners request)
+        {
+            return bannerRepository.EditBanner(request);
+        }
+
+        public Banners GetBannerById(int id)
+        {
+            return bannerRepository.GetBannerById(id);
+        }
+
+        public IList<Banners> GetsBanner()
+        {
+            return bannerRepository.GetsBanner();
+        }
+    }
+}
---
+++ Mp3WebMusic.DAL.INTERFACE/Banner/IBannerRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Mp3WebMusic.DOMAIN.Request_Reponse.Banner;
+
+namespace Mp3WebMusic.DAL.INTERFACE.Banner
+{
+    public interface IBannerRepository
+    {
+        IList<Banners> GetsBanner();
+
+        Banners AddBanner(Banners request);
+        Banners DeleteBanner(int id);
+        Banners EditBanner(Banners request);
+        Banners GetBannerById(int id);
+    }
+}
